d9a898fd0355ab87c35ac15ca4c820a26f4f6ea0
Akash Singh <laughing.buddha.lab@gmail.com>
2025-11-03 20:45:49 +0100

    add pop-up modal

--- frontend/js/aboutContent.js
+++ frontend/js/aboutContent.js
@@ -4,6 +4,6 @@
  */
 
 export const aboutContent = {
-    philosophy: `In the modern world, information and news is extremely cheap and abundant. This creates an information overload upon people that exhausts them without giving them the high quality information that they need. I built this product to solve this problem. People can talk to the app and determine themselves what do they see in their news feed. This app will never seek investment or sponsorship, thus ensuring that only the users have all the say in the design and functionality of this app. Ultimately, this app is an attempt to help the user navigate the current information ecosystem with a greater ease.`
+    philosophy: `In the modern world, news is extremely cheap and abundant. This creates an information overload that exhausts the mind without necessarily providing the high quality information that it needs. I built this product to solve this problem. People can talk to the app and determine by themselves what do they see in their news feed. This app will never seek investment or sponsorship, thus ensuring that only the users have all the say in the design and functionality of this app. Ultimately, this app is an attempt to help the user navigate the current information ecosystem with greater ease.`
 };
 
